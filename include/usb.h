ecb1af29e14dc3173e03b6654686c7af100cd268
ben <ben_hhj@hotmail.com>
2016-01-29 15:06:29 +0800

    fix usb time out

--- include/usb.h
+++ include/usb.h
@@ -39,9 +39,9 @@
  * This is the timeout to allow for submitting an urb in ms. We allow more
  * time for a BULK device to react - some are slow.
  */
-#define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 5000 : 1000)
+//#define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 5000 : 1000)
+#define USB_TIMEOUT_MS(pipe) (usb_pipebulk(pipe) ? 10000 : 1000)
 
-/* device request (setup) */
 struct devrequest {
 	unsigned char	requesttype;
 	unsigned char	request;
